b9a3d07a734e54dbd88a5227f21fe986db555631
Alexander Ryabkov <alexryabkov@gmail.com>
2022-09-08 20:38:52 +0300

    Minor change: re-arrange imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { CurrencyItemComponent } from './components/currency-item/currency-item.component';
 import { CurrenciesComponent } from './components/currencies/currencies.component';
 import { DateTimeComponent } from './components/date-time/date-time.component';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
