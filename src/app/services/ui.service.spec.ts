b9df8e8da057ffc3fe4d274674e2d5a2ddf1782a
Alexander Ryabkov <alexryabkov@gmail.com>
2022-09-10 01:07:06 +0300

    Spec: fix description

--- src/app/services/ui.service.spec.ts
+++ src/app/services/ui.service.spec.ts
@@ -14,13 +14,13 @@ describe('UiService', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should toggle #showExtraCurrencies false<->true on #toggleExtraCurrencies()', () => {
+  it('should toggle #showExtraCurrencies "false -> true" on #toggleExtraCurrencies()', () => {
     service['showExtraCurrencies'] = false;
     service.toggleExtraCurrencies();
     expect(service['showExtraCurrencies']).toBe(true);
   });
 
-  it('should toggle #showExtraCurrencies true<->false on #toggleExtraCurrencies()', () => {
+  it('should toggle #showExtraCurrencies "true -> false" on #toggleExtraCurrencies()', () => {
     service['showExtraCurrencies'] = true;
     service.toggleExtraCurrencies();
     expect(service['showExtraCurrencies']).toBe(false);
