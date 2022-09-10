cb1fe2983a12cd9e44641747feb5f103c323a2a7
Alexander Ryabkov <alexryabkov@gmail.com>
2022-09-10 13:32:27 +0300

    Update env files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiKey: '<api_key>',
+  apiKey: 'undefined',
   apiUrl: 'https://currency-converter-pro1.p.rapidapi.com/latest-rates',
   apiHost: 'currency-converter-pro1.p.rapidapi.com',
 };
