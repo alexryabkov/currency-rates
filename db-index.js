840c3999e92f57a6f69613c1b1bde7d071432e35
Alexander Ryabkov <alexryabkov@gmail.com>
2022-09-10 16:48:22 +0300

    Add fake currencies data script for json-server

--- db-index.js
+++ db-index.js
@@ -10,7 +10,8 @@ const setRatesData = () => {
   const base = "RUB";
   const result = {
     USD: Math.random() * (0.015 - 0.017) + 0.017,
-    EUR: Math.random() * (0.015 - 0.017) + 0.017,
+    // EUR: Math.random() * (0.015 - 0.017) + 0.017,
+    EUR: 0.015,
     GBP: Math.random() * (0.014 - 0.015) + 0.015,
     CNY: Math.random() * (0.112 - 0.115) + 0.115,
     JPY: Math.random() * (1.35 - 2.55) + 2.55,
@@ -31,3 +32,22 @@ server.get("/currencies", (req, res) => {
 server.listen(5000, () => {
   console.log("JSON Server is running");
 });
+
+//  Some static JSON data, just for the reference
+// ----------------------------------------------
+// {
+//   "currencies": {
+//     "success": true,
+//     "timestamp": 1662582540824,
+//     "date": "2022-09-07",
+//     "base": "RUB",
+//     "result": {
+//       "USD": 0.0163532322,
+//       "EUR": 0.0163387596,
+//       "GBP": 0.0141818991,
+//       "CNY": 0.1139182508,
+//       "JPY": 2.3502967526,
+//       "TRY": 0.29808271
+//     }
+//   }
+// }
