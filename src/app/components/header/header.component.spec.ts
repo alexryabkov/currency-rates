b46fa0c765f722b8fed8b49887df04bfe0cc706f
Alexander Ryabkov <alexryabkov@gmail.com>
2022-09-10 12:21:41 +0300

    Add a few tests for Header

--- src/app/components/header/header.component.spec.ts
+++ src/app/components/header/header.component.spec.ts
@@ -5,19 +5,34 @@ import { HeaderComponent } from './header.component';
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let titleElem: HTMLElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
-    })
-    .compileComponents();
+      declarations: [HeaderComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
+    titleElem = fixture.nativeElement.querySelector('.title');
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have as title "Currency Rates to RUB"', () => {
+    expect(component.title).toEqual('Currency Rates to RUB');
+  });
+
+  it('should display original title "Currency Rates to RUB"', () => {
+    expect(titleElem.textContent).toContain(component.title);
+  });
+
+  it('should display title "Test Title"', () => {
+    component.title = 'Test Title';
+    fixture.detectChanges();
+    expect(titleElem.textContent).toContain(component.title);
   });
 });
