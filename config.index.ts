9a0732e5fdcb52b8f44d05ea4e3e36e5c9d7522e
Alexander Ryabkov <alexryabkov@gmail.com>
2022-09-10 14:36:51 +0300

    Add prod env config script

---
+++ config.index.ts
@@ -0,0 +1,18 @@
+import { writeFile } from 'fs';
+import { environment } from './src/environments/environment.prod';
+
+const targetPath = './src/environments/environment.prod.ts';
+
+const envConfigFile = `export const environment = {
+  production: true,
+  apiKey: '${process.env['CURRENCY_API_KEY']}',
+  apiUrl: '${environment.apiUrl}',
+  apiHost: '${environment.apiHost}',
+};
+`;
+
+writeFile(targetPath, envConfigFile, 'utf8', (err) => {
+  if (err) {
+    return console.log(err);
+  }
+});
