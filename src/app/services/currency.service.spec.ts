e64906fe8e6a59aad0ba17798bc5bc874bfa6f42
Alexander Ryabkov <alexryabkov@gmail.com>
2022-09-12 10:58:15 +0300

    Re-arrange tests one more time

--- src/app/services/currency.service.spec.ts
+++ src/app/services/currency.service.spec.ts
@@ -23,58 +23,53 @@ describe('CurrencyService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
-});
 
-describe('CurrencyService#requestCurrencyData', () => {
-  let service: CurrencyService;
-  let backend: HttpTestingController;
+  describe('#requestCurrencyData', () => {
+    let backend: HttpTestingController;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
-    });
-    service = TestBed.inject(CurrencyService);
-    backend = TestBed.inject(HttpTestingController);
-  });
-
-  afterEach(() => backend.verify());
-
-  it('should send proper GET request', () => {
-    service.requestCurrencyData().subscribe();
-
-    const testRequest = backend.expectOne(
-      (req: HttpRequest<any>) => req.url === environment.apiUrl
-    );
-    expect(testRequest.request.method).toBe('GET');
-    expect(testRequest.request.params.get('base')).toBe(BASE_CURRENCY);
-    expect(testRequest.request.params.get('symbols')).toBe(
-      ALL_CURRENCIES.join(',')
-    );
-    testRequest.flush({});
-  });
-
-  it('should get proper data', () => {
-    const timestamp = Date.now();
-    const params = new HttpParams()
-      .set('base', BASE_CURRENCY)
-      .set('symbols', ALL_CURRENCIES.join(','));
-    const url = `${environment.apiUrl}?${params.toString()}`;
-    const rates = ALL_CURRENCIES.reduce(
-      (obj, key) => Object.assign(obj, { [key]: 10 }),
-      {}
-    );
-    const response: FetchedCurrencyData = {
-      success: true,
-      timestamp,
-      date: new Date(timestamp).toISOString().split('T')[0],
-      base: BASE_CURRENCY,
-      rates,
-    };
-
-    service.requestCurrencyData().subscribe((next) => {
-      expect(next).toEqual(response);
+    beforeEach(() => {
+      backend = TestBed.inject(HttpTestingController);
     });
 
-    backend.match({ url, method: 'GET' })[0].flush(response);
+    afterEach(() => backend.verify());
+
+    it('should send proper GET request', () => {
+      service.requestCurrencyData().subscribe();
+
+      const testRequest = backend.expectOne(
+        (req: HttpRequest<any>) => req.url === environment.apiUrl
+      );
+      expect(testRequest.request.method).toBe('GET');
+      expect(testRequest.request.params.get('base')).toBe(BASE_CURRENCY);
+      expect(testRequest.request.params.get('symbols')).toBe(
+        ALL_CURRENCIES.join(',')
+      );
+      testRequest.flush({});
+    });
+
+    it('should get proper data', () => {
+      const timestamp = Date.now();
+      const params = new HttpParams()
+        .set('base', BASE_CURRENCY)
+        .set('symbols', ALL_CURRENCIES.join(','));
+      const url = `${environment.apiUrl}?${params.toString()}`;
+      const rates = ALL_CURRENCIES.reduce(
+        (obj, key) => Object.assign(obj, { [key]: 10 }),
+        {}
+      );
+      const response: FetchedCurrencyData = {
+        success: true,
+        timestamp,
+        date: new Date(timestamp).toISOString().split('T')[0],
+        base: BASE_CURRENCY,
+        rates,
+      };
+
+      service.requestCurrencyData().subscribe((next) => {
+        expect(next).toEqual(response);
+      });
+
+      backend.match({ url, method: 'GET' })[0].flush(response);
+    });
   });
 });
--- src/app/services/ui.service.spec.ts
+++ src/app/services/ui.service.spec.ts
@@ -13,25 +13,18 @@ describe('UiService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
-});
 
-describe('UiService#toggleExtraCurrencies', () => {
-  let service: UiService;
+  describe('#toggleExtraCurrencies', () => {
+    it('should toggle #showExtraCurrencies "false -> true" on call', () => {
+      service['showExtraCurrencies'] = false;
+      service.toggleExtraCurrencies();
+      expect(service['showExtraCurrencies']).toBe(true);
+    });
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({ providers: [UiService] });
-    service = TestBed.inject(UiService);
-  });
-
-  it('should toggle #showExtraCurrencies "false -> true" on call', () => {
-    service['showExtraCurrencies'] = false;
-    service.toggleExtraCurrencies();
-    expect(service['showExtraCurrencies']).toBe(true);
-  });
-
-  it('should toggle #showExtraCurrencies "true -> false" on call', () => {
-    service['showExtraCurrencies'] = true;
-    service.toggleExtraCurrencies();
-    expect(service['showExtraCurrencies']).toBe(false);
+    it('should toggle #showExtraCurrencies "true -> false" on call', () => {
+      service['showExtraCurrencies'] = true;
+      service.toggleExtraCurrencies();
+      expect(service['showExtraCurrencies']).toBe(false);
+    });
   });
 });
